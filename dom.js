c0760e72a394fe749c6d2461e4f328aa21f4736f
Ayushshadi <51690555+Ayushshadi@users.noreply.github.com>
2023-07-06 15:17:24 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -4,3 +4,9 @@ header.style.borderBottom='solid 3px #000';
 var item=document.getElementsByClassName('title');
 item[0].style.fontWeight= 'bold';
 item[0].style.backgroundColor='green';
+var list=document.getElementsByClassName('list-group-item');
+list[2].style.backgroundColor='green';
+for(var i=0;i<list.length;i++)
+{
+    list[i].style.fontWeight='bold';
+}
