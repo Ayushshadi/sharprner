df83f79addc04a7e8e81df12c12146f333614a5c
Ayushshadi <51690555+Ayushshadi@users.noreply.github.com>
2023-07-06 14:53:35 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,6 @@
+var head=document.getElementById('header-title');
+var header=document.getElementById('main-header');
+header.style.borderBottom='solid 3px #000';
+var item=document.getElementsByClassName('title');
+item[0].style.fontWeight= 'bold';
+item[0].style.backgroundColor='green';
