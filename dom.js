7e043db7f0893b85927c49b34f9cc1df2e2ca5db
Ayushshadi <51690555+Ayushshadi@users.noreply.github.com>
2023-07-07 15:11:49 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -21,3 +21,53 @@ for(var i=0;i<odd.length;i++)
     odd[i].style.backgroundColor='red';
     even[i].style.backgroundColor='blue';
 }
+var itemlist= document.querySelector('#items');
+//parentelement
+//console.log(itemlist.parentElement);
+itemlist.parentElement.style.backgroundColor='#f4f4f4';
+
+//firstchild
+itemlist.firstChild.textContent='hello 1';
+
+//firstelementchild
+itemlist.firstElementChild.style.backgroundColor='#f3f3f3';
+
+//lastelementchild
+itemlist.lastElementChild.textContent='ayush';
+
+//nextsibling
+console.log(itemlist.nextSibling);
+
+//nextelementsibling
+console.log(itemlist.nextElementSibling);
+
+//preivoussibling
+console.log(itemlist.previousSibling);
+
+//preivouselementsibling
+console.log(itemlist.previousElementSibling);
+//create element
+
+//create a div
+var newdiv=document.createElement('div');
+
+//add class
+newdiv.className='ayu';
+
+//add id
+newdiv.id='ayu12';
+
+//add attribute
+newdiv.setAttribute('title','shadi');
+
+//create text node
+var textnode=document.createTextNode('ayush shadi');
+
+//add text to div
+newdiv.appendChild(textnode);
+
+//add to document
+var cont=document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+cont.insertBefore(newdiv,h1);   
+newdiv.style.fontSize='30px';
