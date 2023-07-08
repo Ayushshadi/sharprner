b9f5cbeae48f87348f034139aa687a7310a7f154
Ayushshadi <51690555+Ayushshadi@users.noreply.github.com>
2023-07-08 18:05:34 +0530

    Update multi_user.js

--- multi_user.js
+++ multi_user.js
@@ -27,5 +27,17 @@ function test(e)
                 localStorage.removeItem(userdata.umail);
             });
             child.appendChild(delbtn);
+              var editbtn=document.createElement('button');
+            editbtn.style.backgroundColor='green';
+            editbtn.textContent='edit';
+            editbtn.addEventListener('click',function(){
+                document.getElementById('name').value=userdata.uname;
+                document.getElementById('mail').value=userdata.umail;
+                document.getElementById('number').value=userdata.unum;
+                child.style.display='none';
+                localStorage.removeItem(userdata.umail);
+            });
+            child.appendChild(editbtn);
         }
+
 }   
