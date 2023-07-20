635bbbd7c03440c6ec04a5fe137e082aa55143ad
Ayushshadi <51690555+Ayushshadi@users.noreply.github.com>
2023-07-20 13:49:14 +0530

    Update appointment.js

--- appointment.js
+++ appointment.js
@@ -23,52 +23,66 @@ function additem(e)
             document.body.innerHTML=document.body.innerHTML+"<h4>Something went wrong</h4>";
             console.log(err);
         })
-    function show(userdata)
-    {
-        var newitem=userdata.na+' '+userdata.m+' '+userdata.no;
-    //list create karna hai
-    var li=document.createElement('li');
-
-    //li ko class name dena hai
-    li.className='list-group-item';
-    
-    //li ko name dena hai jo user ne bola hai
-    li.appendChild(document.createTextNode(newitem));
-
-    //delete btn bhi bana hai
-    var deletebtn=document.createElement('button');
-
-    //delete btn ko class name dena hai
-   deletebtn.className='btn btn-danger btn-sm float-right delete';
-
-    
-    //delete btn ko name dena hai jo user ne bola hai
-   deletebtn.appendChild(document.createTextNode('X'));
-
-    //delete btn ko li m add karna hai
-    li.appendChild(deletebtn);
-    deletebtn.addEventListener('click',function(){
-        li.style.display='none';
-       //localStorage.removeItem(userdata.m);
-    });
-    var editbtn=document.createElement('button');
-
-    //delete btn ko class name dena hai
-   editbtn.className='btn btn-success btn-sm float-right delete';
-
-    
-    //delete btn ko name dena hai jo user ne bola hai
-   editbtn.appendChild(document.createTextNode('Edit'));
-   editbtn.addEventListener('click',function(){
-    document.getElementById('name').value=userdata.na;
-    document.getElementById('email').value=userdata.m;
-    document.getElementById('number').value=userdata.no;
-    li.style.display='none';
-    //localStorage.removeItem(userdata.m);
-});
-    //delete btn ko li m add karna hai
-    li.appendChild(editbtn);
-    //jo new item hai usko list m add karna hai
-    itemlist.appendChild(li);
-}
     }
+window.onload=function()
+{
+    axios.get("https://crudcrud.com/api/36ffa99187d64437acf27a517be9508c/appointment")
+      .then(res =>{
+        for(var i=0;i<res.data.length;i++)
+        {
+            show(res.data[i]);
+        }
+      })
+      .catch(err=>{
+        document.body.innerHTML=document.body.innerHTML+"<h4>Something went wrong</h4>";
+        console.log(err);
+      }); 
+}
+function show(userdata)
+{
+    var newitem=userdata.na+' '+userdata.m+' '+userdata.no;
+//list create karna hai
+var li=document.createElement('li');
+
+//li ko class name dena hai
+li.className='list-group-item';
+
+//li ko name dena hai jo user ne bola hai
+li.appendChild(document.createTextNode(newitem));
+
+//delete btn bhi bana hai
+var deletebtn=document.createElement('button');
+
+//delete btn ko class name dena hai
+deletebtn.className='btn btn-danger btn-sm float-right delete';
+
+
+//delete btn ko name dena hai jo user ne bola hai
+deletebtn.appendChild(document.createTextNode('X'));
+
+//delete btn ko li m add karna hai
+li.appendChild(deletebtn);
+deletebtn.addEventListener('click',function(){
+    li.style.display='none';
+   //localStorage.removeItem(userdata.m);
+});
+var editbtn=document.createElement('button');
+
+//delete btn ko class name dena hai
+editbtn.className='btn btn-success btn-sm float-right delete';
+
+
+//delete btn ko name dena hai jo user ne bola hai
+editbtn.appendChild(document.createTextNode('Edit'));
+editbtn.addEventListener('click',function(){
+document.getElementById('name').value=userdata.na;
+document.getElementById('email').value=userdata.m;
+document.getElementById('number').value=userdata.no;
+li.style.display='none';
+//localStorage.removeItem(userdata.m);
+});
+//delete btn ko li m add karna hai
+li.appendChild(editbtn);
+//jo new item hai usko list m add karna hai
+itemlist.appendChild(li);
+}
